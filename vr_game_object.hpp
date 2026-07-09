a5b736cfa65a6750059fde4519c29a10a688581d
Anna Chen <annachen@Annas-MacBook-Air.local>
2026-07-09 08:45:21 -0700

    Tutorial 12: from 2D to 3D

--- vr_game_object.hpp
+++ vr_game_object.hpp
@@ -2,26 +2,49 @@
 
 #include "vr_model.hpp"
 
+// libs
+#include <glm/gtc/matrix_transform.hpp>
+
 // std
 #include <memory>
 
 namespace vr
 {
-    struct Transform2dComponent
+    struct TransformComponent
     {
-        glm::vec2 translation{}; // (position offset)
-        glm::vec2 scale{1.f, 1.f};
-        float rotation;
+        glm::vec3 translation{}; // (position offset)
+        glm::vec3 scale{1.f, 1.f, 1.f};
+        glm::vec3 rotation{};
 
-        glm::mat2 mat2()
+        glm::mat4 mat4() {
+            const float c3 = glm::cos(rotation.z);
+    const float s3 = glm::sin(rotation.z);
+    const float c2 = glm::cos(rotation.x);
+    const float s2 = glm::sin(rotation.x);
+    const float c1 = glm::cos(rotation.y);
+    const float s1 = glm::sin(rotation.y);
+    return glm::mat4{
         {
-            const float s = glm::sin(rotation);
-            const float c = glm::cos(rotation);
-            glm::mat2 rotMatrix{{c, s}, {-s, c}};
-
-            glm::mat2 scaleMat{{scale.x, .0f}, {.0f, scale.y}};
-            return rotMatrix * scaleMat;
+            scale.x * (c1 * c3 + s1 * s2 * s3),
+            scale.x * (c2 * s3),
+            scale.x * (c1 * s2 * s3 - c3 * s1),
+            0.0f,
+        },
+        {
+            scale.y * (c3 * s1 * s2 - c1 * s3),
+            scale.y * (c2 * c3),
+            scale.y * (c1 * c3 * s2 + s1 * s3),
+            0.0f,
+        },
+        {
+            scale.z * (c2 * s1),
+            scale.z * (-s2),
+            scale.z * (c1 * c2),
+            0.0f,
+        },
+        {translation.x, translation.y, translation.z, 1.0f}};
         }
+        
     };
 
     class VrGameObject
@@ -44,7 +67,7 @@ namespace vr
 
         std::shared_ptr<VrModel> model{};
         glm::vec3 color{};
-        Transform2dComponent transform2d{};
+        TransformComponent transform{};
 
     private:
         VrGameObject(id_t objId) : id{objId} {}
